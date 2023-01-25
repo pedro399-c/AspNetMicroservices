e8e5c11f2a6275916d3a1a1a2b715c51f2f6b42a
pedro.rocha-gomes.ext <pedro.rocha-gomes.ext@siemens.com>
2023-01-25 16:57:41 +0000

    [00000] Added basket api.

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+
+        public ShoppingCart(string userName)
+        {
+            this.Username = userName;
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            this._cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        public Task DeleteBasket(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await this._cache.GetStringAsync(username);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await this._cache.SetStringAsync(basket.Username, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.Username);
+        }
+    }
+}
