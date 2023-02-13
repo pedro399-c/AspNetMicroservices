5b63e48c685d8c632c5376c5f3b1038d64bc92e4
pedro.rocha-gomes.ext <pedro.rocha-gomes.ext@siemens.com>
2023-02-13 18:11:15 +0000

    [00000] Added ocelot api project.

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+var app = builder.Build();
+await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
